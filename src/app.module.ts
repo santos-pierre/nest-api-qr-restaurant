cfa505e1d0ff715d15b893b182cf908a2079e49d
Santos Pierre <santospierre1305@gmail.com>
2022-04-27 20:37:03 +0200

    Typeorm connection configuration 🔧

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: process.env.DATABASE_HOST,
+			port: parseInt(process.env.DATABASE_PORT),
+			username: process.env.DATABASE_USERNAME,
+			password: process.env.DATABASE_PASSWORD,
+			database: process.env.DATABASE_NAME,
+			synchronize: true,
+			autoLoadEntities: true,
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
